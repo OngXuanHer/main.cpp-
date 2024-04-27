e6681279dc364c0a6890a96f719a5fb41581653d
OngXuanHer <167597412+OngXuanHer@users.noreply.github.com>
2024-04-27 11:05:36 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+std::bool << "secs_per_min ="<<60<<"\n";";
+ std::bool << "mins_per_hour ="<<60<<"\n";";
+type name = value;
+  value = secs_per_min
+  secs_per_min = 60
+  print(secs_per_min)
+  value = mins_per_hour
+  mins_per_hour = 60
